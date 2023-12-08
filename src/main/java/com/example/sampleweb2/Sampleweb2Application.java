399fd12bede1ba3eed57a46f222ba6f410214330
ShaikJdk <kannak07@gmail.com>
2023-12-08 19:41:38 +0530

    changing jar to war

--- src/main/java/com/example/sampleweb2/Sampleweb2Application.java
+++ src/main/java/com/example/sampleweb2/Sampleweb2Application.java
@@ -2,12 +2,18 @@ package com.example.sampleweb2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Sampleweb2Application {
+public class Sampleweb2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sampleweb2Application.class, args);
 	}
 
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(Sampleweb2Application.class);
+	    }
 }
